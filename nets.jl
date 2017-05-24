d62b69f9d5e1a7bc8909578ffcedbcecb277f532
Robin Deits <robin.deits@gmail.com>
2017-05-24 15:18:18 -0700

    fix compatibility with reversediff tape recording

--- nets.jl
+++ nets.jl
@@ -1,5 +1,6 @@
 module Nets
 
+using Parameters
 import ReverseDiff
 using MLDataPattern: batchview, shuffleobs
 
@@ -10,71 +11,159 @@ function viewblocks{T <: NTuple}(data::AbstractArray, shapes::AbstractVector{T})
     [reshape(view(data, starts[i]:(starts[i+1] - 1)), shapes[i]) for i in 1:length(shapes)]
 end
 
-type PANet{Sensitive}
+type Net
     shapes::Vector{NTuple{2, Int}}
 end
 
-nweights(net::PANet) = sum(prod, net.shapes)
-nbiases(net::PANet) = sum(first, net.shapes)
-nparams(net::PANet) = nweights(net) + nbiases(net)
-Base.rand(net::PANet) = rand(nparams(net))
-Base.randn(net::PANet) = randn(nparams(net))
+Net(widths::Vector{<:Integer}) = Net(collect(zip(widths[2:end], widths[1:end-1])))
 
-function predict(net::PANet{true}, params::AbstractVector, x::AbstractVector)
+nweights(net::Net) = sum(prod, net.shapes)
+nbiases(net::Net) = sum(first, net.shapes)
+nparams(net::Net) = nweights(net) + nbiases(net)
+ninputs(net::Net) = net.shapes[1][2]
+noutput(net::Net) = net.shapes[end][1]
+Base.rand(net::Net) = rand(nparams(net))
+Base.randn(net::Net) = randn(nparams(net))
+
+@ReverseDiff.forward relu(y) = y >= 0 ? y : 0.1 * y
+@ReverseDiff.forward relu_sensitivity(y, j) = y >= 0 ? j : 0.1 * j
+
+function predict_sensitivity(net::Net, params::AbstractVector, x::AbstractVector)
     weights = viewblocks(params, net.shapes)
     biases = viewblocks(@view(params[(nweights(net) + 1):end]), head.(net.shapes))
-    y = similar(x, Base.promote_eltype(params, x), (length(x), 1))
-    y .= x
-    J = eye(eltype(y), length(x))
-    for i in 1:(length(net.shapes) - 1)
-        w = weights[i]
-        y = w * y .+ biases[i]
-        J = w * J
-        z = [yy >= 0 ? 1.0 : 0.1 for yy in y]
-        y = y .* z
-        J = J .* z
+    J = similar(weights[1])
+    y = weights[1] * x .+ biases[1]
+    J .= weights[1]
+    for i in 2:length(net.shapes)
+        y = relu.(y)
+        J = relu_sensitivity.(y, J)
+
+        y = weights[i] * y + biases[i]
+        J = weights[i] * J
     end
-    w = weights[end]
-    vcat(vec(w * y), vec(w * J))
+    vcat(vec(y), vec(J))
 end
 
-function predict(net::PANet{false}, params::AbstractVector, x::AbstractVector)
+function predict(net::Net, params::AbstractVector, x::AbstractVector)
     weights = viewblocks(params, net.shapes)
     biases = viewblocks(@view(params[(nweights(net) + 1):end]), head.(net.shapes))
-    y = similar(x, Base.promote_eltype(params, x), (length(x), 1))
-    y .= x
-    for i in 1:(length(net.shapes) - 1)
-        w = weights[i]
-        y = w * y .+ biases[i]
-        z = [yy >= 0 ? 1.0 : 0.1 for yy in y]
-        y = y .* z
+    y = weights[1] * x .+ biases[1]
+    for i in 2:length(net.shapes)
+        y = relu.(y)
+
+        y = weights[i] * y + biases[i]
     end
-    w = weights[end]
-    vec(w * y)
+    vec(y)
 end
 
-function sgd!(loss, params, data, lr=0.01, momentum=0.8, batchsize=1)
+@with_kw type SGDOpts
+    learning_rate::Float64 = 0.01
+    momentum::Float64 = 0.0
+    batchsize::Int = 1
+    learning_decay = 1.0
+end
+
+function sgd!(loss, params, data, opts::SGDOpts=SGDOpts())
     last_descent = zeros(params)
     dw = zeros(params)
-    dw_sample = zeros(dw)
-    sample_weight = 1 / batchsize
-    for batch in batchview(shuffleobs(data), batchsize)
+    sample_weight = 1 / opts.batchsize
+    n_in = length(data[1][1])
+    n_out = length(data[1][2])
+    loss_tape = ReverseDiff.compile(ReverseDiff.GradientTape(loss, 
+        (params, randn(n_in), randn(n_out))))
+    gradient_result = (similar(params), zeros(n_in), zeros(n_out))
+    learning_rate = opts.learning_rate
+    for batch in batchview(shuffleobs(data), opts.batchsize)
         dw .= 0
         for (x, y) in batch
-            ReverseDiff.gradient!(dw_sample, w -> loss(w, x, y), params)
-            if any(isnan.(dw_sample))
-                @show params x y dw_sample
-                error("nan")
-            end
-            dw .+= sample_weight .* dw_sample
+            ReverseDiff.gradient!(gradient_result, loss_tape, (params, x, y))
+            @assert gradient_result[1] ≈ ReverseDiff.gradient(w -> loss(w, x, y), params)
+            dw .+= sample_weight .* gradient_result[1]
         end
         for i in eachindex(params)
-            v = lr * dw[i] + momentum * last_descent[i]
+            v = learning_rate * dw[i] + opts.momentum * last_descent[i]
             params[i] -= v
             last_descent[i] = v
         end
+        learning_rate *= opts.learning_decay
     end
     params
 end
+
+using Base.Test
+
+@testset "feedforward" begin
+    shapes = [(1, 1), (1, 1)]
+    net = Net(shapes)
+    srand(1)
+    for i in 1:100
+        params = randn(net)
+        x = randn(1)
+        @test @inferred(predict(net, params, x))[1] == @inferred(predict_sensitivity(net, params, x)[1])
+
+        dx = [1e-3]
+        y = predict_sensitivity(net, params, x)
+        J = y[2:2]
+        @test predict(net, params, x .+ dx) ≈ y[1:1] .+ J .* dx
+    end
+end
+
+@testset "random shapes" begin
+    srand(10)
+    for i in 1:100
+        widths = [rand(1:5) for i in 1:4]
+        net = Net(widths)
+        x = randn(widths[1])
+        params = randn(net)
+        @test predict(net, params, x) == predict_sensitivity(net, params, x)[1:widths[end]]
+        for i in 1:length(x)
+            dx = zeros(x)
+            dx[i] = 1e-3
+            out = predict_sensitivity(net, params, x)
+            y = out[1:widths[end]]
+            J = reshape(out[widths[end]+1:end], widths[end], length(x))
+            @test predict(net, params, x .+ dx) ≈ y .+ J * dx
+        end
+    end
+end
+
+@testset "compiled jacobian" begin
+    srand(20)
+    for i in 1:10
+        widths = [rand(1:5) for i in 1:4]
+        net = Net(widths)
+        g = (params, x) -> sum(predict(net, params, x))
+        params = randn(net)
+        x = randn(widths[1])
+        g_tape = ReverseDiff.compile(ReverseDiff.GradientTape(g, (params, x)))
+        results = (similar(params), similar(x))
+
+        for i in 1:50
+            x = randn(widths[1])
+            params = randn(net)
+            ReverseDiff.gradient!(results, g_tape, (params, x))
+            @test all(results .≈ ReverseDiff.gradient(g, (params, x)))
+        end
+    end
+
+    for i in 1:10
+        widths = [rand(1:5) for i in 1:4]
+        net = Net(widths)
+        g = (params, x) -> sum(predict_sensitivity(net, params, x))
+        params = randn(net)
+        x = randn(widths[1])
+        g_tape = ReverseDiff.compile(ReverseDiff.GradientTape(g, (params, x)))
+        results = (similar(params), similar(x))
+
+        for i in 1:50
+            x = randn(widths[1])
+            params = randn(net)
+            ReverseDiff.gradient!(results, g_tape, (params, x))
+            @test all(results .≈ ReverseDiff.gradient(g, (params, x)))
+        end
+    end
+end
+
+
 
 end
