f9933f5ee23b9502fc777b3c79ee05d582caa3c3
Robin Deits <robin.deits@gmail.com>
2017-05-25 02:12:35 -0700

    adapt explicit_network methods into nets.jl

--- nets.jl
+++ nets.jl
@@ -1,8 +1,12 @@
 module Nets
 
-using Parameters
+using Parameters: @with_kw
 import ReverseDiff
 using MLDataPattern: batchview, shuffleobs
+using Polyhedra
+using CDDLib
+using DiffBase
+using ForwardDiff
 
 head(t::Tuple) = tuple(t[1])
 
@@ -25,35 +29,135 @@ noutput(net::Net) = net.shapes[end][1]
 Base.rand(net::Net) = rand(nparams(net))
 Base.randn(net::Net) = randn(nparams(net))
 
-@ReverseDiff.forward relu(y) = y >= 0 ? y : 0.1 * y
-@ReverseDiff.forward relu_sensitivity(y, j) = y >= 0 ? j : 0.1 * j
+immutable Params{T, M<:AbstractMatrix{T}, V<:AbstractVector{T}}
+    weights::Vector{M}
+    biases::Vector{V}
+end
 
-function predict_sensitivity(net::Net, params::AbstractVector, x::AbstractVector)
+Params{T, M<:AbstractMatrix{T}, V<:AbstractVector{T}}(weights::Vector{M}, biases::Vector{V}) = 
+    Params{T, M, V}(weights, biases)
+
+function Params(net::Net, params::AbstractVector)
     weights = viewblocks(params, net.shapes)
     biases = viewblocks(@view(params[(nweights(net) + 1):end]), head.(net.shapes))
-    J = similar(weights[1])
-    y = weights[1] * x .+ biases[1]
-    J .= weights[1]
-    for i in 2:length(net.shapes)
-        y = relu.(y)
-        J = relu_sensitivity.(y, J)
+    Params(weights, biases)
+end
 
-        y = weights[i] * y + biases[i]
-        J = weights[i] * J
+ninputs(p::Params) = size(p.weights[1], 2)
+noutputs(p::Params) = size(p.weights[end], 1)
+
+@ReverseDiff.forward leaky_relu(y) = y >= 0 ? y : 0.1 * y
+@ReverseDiff.forward leaky_relu_sensitivity(y, j) = y >= 0 ? j : 0.1 * j
+
+predict_sensitivity(net::Net, params::AbstractVector, x::AbstractVector) = 
+    predict_sensitivity(Params(net, params), x)
+
+function predict_sensitivity(params::Params, x::AbstractVector)
+    J = similar(params.weights[1])
+    y = params.weights[1] * x .+ params.biases[1]
+    J .= params.weights[1]
+    for i in 2:length(params.weights)
+        y = leaky_relu.(y)
+        J = leaky_relu_sensitivity.(y, J)
+        y = params.weights[i] * y + params.biases[i]
+        J = params.weights[i] * J
     end
     vcat(vec(y), vec(J))
 end
 
-function predict(net::Net, params::AbstractVector, x::AbstractVector)
-    weights = viewblocks(params, net.shapes)
-    biases = viewblocks(@view(params[(nweights(net) + 1):end]), head.(net.shapes))
-    y = weights[1] * x .+ biases[1]
-    for i in 2:length(net.shapes)
-        y = relu.(y)
+predict(net::Net, params::AbstractVector, x::AbstractVector) = 
+    predict(Params(net, params), x)
 
-        y = weights[i] * y + biases[i]
+function predict(params::Params, x::AbstractVector)
+    y = params.weights[1] * x .+ params.biases[1]
+    for i in 2:length(params.weights)
+        y = leaky_relu.(y)
+        y = params.weights[i] * y + params.biases[i]
     end
     vec(y)
+end
+
+leaky_relu(y, active::Bool) = active ? y : 0.1 * y
+
+function predict_and_record(params::Params, x::AbstractVector, relu_activations=nothing)
+    y = params.weights[1] * x .+ params.biases[1]
+    result = [copy(y)]
+    for i in 2:length(params.weights)
+        if typeof(relu_activations) == Void
+            y = leaky_relu.(y)
+        else
+            y = leaky_relu.(y, relu_activations[i - 1])
+        end
+        y = params.weights[i] * y + params.biases[i]
+        push!(result, copy(y))
+    end
+    result
+end
+
+function relu_constraints{T}(params::Params{T}, relu_activations)
+    x = zeros(T, ninputs(params))
+    f = x -> vcat(predict_and_record(params, x, relu_activations)[1:end-1]...)
+    y = f(x)
+    out = DiffBase.DiffResult(similar(y), similar(y, length(y), length(x)))
+    ForwardDiff.jacobian!(out, f, x)
+    v = DiffBase.value(out)
+    J = DiffBase.jacobian(out)
+    # y = v + J * x
+    # we want to ensure that y remains on the current side of 0
+    # for each relu. 
+    # y_i >= 0 if relu i is active
+    # y_i <= 0 otherwise
+    # 
+    # -y_i <= 0 if active
+    #  y_i <= 0 if inactive
+    #
+    A = J
+    b = -v
+    i = 1
+    for a in relu_activations
+        for ai in a
+            if ai
+                A[i, :] .*= -1
+                b[i] .*= -1
+            end
+            i += 1
+        end
+    end
+    SimpleHRepresentation(A, b)
+end
+
+function explore{T}(params::Params{T}, bounds, start::AbstractVector)
+    record = predict_and_record(params, start)
+    state = [x .>= 0 for x in record[1:end-1]]
+    constr = relu_constraints(params, state)
+
+    results = Dict{typeof(state), typeof(constr)}()
+
+    active_set = Set([state])
+
+    while !isempty(active_set)
+        new_active_set = Set{typeof(state)}()
+        for state in active_set
+            constr = relu_constraints(params, state)
+            p = intersect(constr, bounds)
+            if isempty(SimpleVRepresentation(vrep(polyhedron(p, CDDLibrary(:exact)))).V)
+                continue
+            end
+            results[state] = p
+            for J in eachindex(state)
+                layerstate = state[J]
+                for I in eachindex(layerstate)
+                    newstate = deepcopy(state)
+                    newstate[J][I] = !newstate[J][I]
+                    if !haskey(results, newstate)
+                        push!(new_active_set, newstate)
+                    end
+                end
+            end
+        end
+        active_set = new_active_set
+    end
+    results
 end
 
 @with_kw type SGDOpts
