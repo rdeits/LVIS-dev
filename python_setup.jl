c504ed6aa655de0e50bf64420ba8bf10c049f1dc
Robin Deits <robin.deits@gmail.com>
2017-05-25 17:20:16 -0700

    update setup script for mac

--- python_setup.jl
+++ python_setup.jl
@@ -1,6 +1,12 @@
 using PyCall
 @pyimport pip
 pip.main(["install", "-r", "py-mpc/requirements.txt"])
-cd("$(ENV["HOME"])/apps/gurobi701/linux64") do
-    run(`$(PyCall.python) setup.py install`)
+if is_linux()
+    cd("$(ENV["HOME"])/apps/gurobi701/linux64") do
+        run(`$(PyCall.python) setup.py install`)
+    end
+else
+    cd("/Library/gurobi702/mac64") do
+        run(`$(PyCall.python) setup.py install`)
+    end
 end
