287bf2a39ed8d5a6730622157d01da3f9c035139
Robin Deits <robin.deits@gmail.com>
2017-07-19 19:04:13 -0400

    lower stiffness results in much easier MIQP models

--- experiments/2017-07-16-box-atlas-indicators/boxatlas.jl
+++ experiments/2017-07-16-box-atlas-indicators/boxatlas.jl
@@ -22,8 +22,8 @@ end
 @with_kw mutable struct BoxAtlas{T}
     position_limits::Dict{Body, SimpleHRepresentation{2, T}} = Dict(
         Trunk=>from_bounds([-1.0, 0], [1.0, 2.0]),
-        LeftFoot=>from_bounds([0.0, -0.7], [0.4, -0.3]),
-        RightFoot=>from_bounds([-0.4, -0.7], [0.0, -0.3]),
+        LeftFoot=>from_bounds([0.0, -1.0], [0.4, -0.5]),
+        RightFoot=>from_bounds([-0.4, -1.0], [0.0, -0.5]),
         LeftHand=>from_bounds([0.2, -0.1], [0.6, 0.3]),
         RightHand=>from_bounds([-0.6, -0.1], [-0.2, 0.3])
     )
@@ -143,20 +143,58 @@ Base.done(h::HRepIter, i) = donehrep(h.p, i)
 Base.next(h::HRepIter, i) = nexthrep(h.p, i)
 Base.length(h::HRepIter) = length(h.p)
 
+struct StateUpdate{T}
+    input_forces::Dict{Body, SVector{2, T}}
+    joint_limit_forces::Dict{Body, SVector{2, T}}
+    gravity_forces::Dict{Body, SVector{2, T}}
+    ground_contact_forces::Dict{Body, SVector{2, T}}
+    wall_contact_forces::Dict{Body, SVector{2, T}}
+    damping_forces::Dict{Body, SVector{2, T}}
+
+    function StateUpdate{T}() where {T}
+        bodies = [Trunk, LeftFoot, RightFoot, LeftHand, RightHand]
+        up = new{T}(
+           Dict([body=>zeros(SVector{2, T}) for body in bodies]),
+           Dict([body=>zeros(SVector{2, T}) for body in bodies]),
+           Dict([body=>zeros(SVector{2, T}) for body in bodies]),
+           Dict([body=>zeros(SVector{2, T}) for body in bodies]),
+           Dict([body=>zeros(SVector{2, T}) for body in bodies]),
+           Dict([body=>zeros(SVector{2, T}) for body in bodies]),
+        )
+        for body in bodies
+            up.input_forces[body] = zeros(SVector{2, T})
+            up.joint_limit_forces[body] = zeros(SVector{2, T})
+            up.gravity_forces[body] = zeros(SVector{2, T})
+            up.ground_contact_forces[body] = zeros(SVector{2, T})
+            up.wall_contact_forces[body] = zeros(SVector{2, T})
+            up.damping_forces[body] = zeros(SVector{2, T})
+        end
+        up
+    end
+end
+
+function accelerations(robot::BoxAtlas, up::StateUpdate{T}) where {T}
+    bodies = [Trunk, LeftFoot, RightFoot, LeftHand, RightHand]
+    accelerations = Dict([body=>zeros(SVector{2, T}) for body in bodies])
+    for body in bodies
+        accelerations[body] = sum(getfield(up, field)[body] for field in fieldnames(up)) ./ robot.masses[body]
+    end
+    for body in [LeftFoot, RightFoot, LeftHand, RightHand]
+        # Non-inertial frame
+        accelerations[body] -= accelerations[Trunk]
+    end
+    accelerations
+end
+
 function update(model::BoxAtlas, x::State{T1}, u::Input{T2}) where {T1, T2}
     Tnext = Base.promote_op(+, T1, T2)
     xnext = similar(x, Tnext)
-    acceleration = Dict{Body, SVector{2, Tnext}}()
 
-    # Apply joint forces
-    acceleration[Trunk] = zero(SVector{2, Tnext})
-    for body in keys(u.force)
-        acceleration[body] = u.force[body] ./ model.masses[body]
-        acceleration[Trunk] -= u.force[body] ./ model.masses[Trunk]
+    up = StateUpdate{Tnext}()
+    for (body, force) in u.force
+        up.input_forces[body] = force
+        up.input_forces[Trunk] -= force
     end
-
-    # TODO: separate components of acceleration so we can see which one is
-    # misbehaving
 
     # Apply soft joint limits
     for body in keys(u.force)
@@ -169,30 +207,39 @@ function update(model::BoxAtlas, x::State{T1}, u::Input{T2}) where {T1, T2}
                 zeros(face.a)
             end
         end
-        acceleration[body] += force ./ model.masses[body]
-        acceleration[Trunk] += -force ./ model.masses[Trunk]
+        up.joint_limit_forces[body] = force
+        up.joint_limit_forces[Trunk] -= force
     end
 
     # Gravity
     for body in [Trunk, keys(u.force)...]
-        acceleration[body] += SVector(0, -model.gravity)
+        up.gravity_forces[body] = SVector(0, -model.gravity)
     end
 
     # Ground contact
     for body in (LeftFoot, RightFoot)
         separation = x.position[Trunk][2] + x.position[body][2]
-        acceleration[body] += @disjunction if separation <= 0
-            (-separation .* model.stiffness .* SVector(0., 1) - model.viscous_friction .* (x.velocity[Trunk] .+ x.velocity[body]) .* SVector(1., 1)) ./ model.masses[body]
+        up.ground_contact_forces[body] = @disjunction if separation <= 0
+            (-separation .* model.stiffness .* SVector(0., 1) - model.viscous_friction .* (x.velocity[Trunk] .+ x.velocity[body]) .* SVector(1., 1))
         else
             zeros(SVector{2, Tnext})
         end
     end
 
+    # Wall contact
+    body = RightHand
+    separation = x.position[Trunk][1] + x.position[body][1]
+    up.wall_contact_forces[body] = @disjunction if separation <= 0
+        (-separation .* model.stiffness .* SVector(1., 0) - model.viscous_friction .* (x.velocity[Trunk] .+ x.velocity[body]) .* SVector(1., 1))
+    else
+        zeros(SVector{2, Tnext})
+    end
+
     # Damping
     for body in [Trunk, keys(u.force)...]
         damping_force = .-model.damping .* x.velocity[body]
-        acceleration[body] += damping_force ./ model.masses[body]
-        acceleration[Trunk] -= damping_force ./ model.masses[Trunk]
+        up.damping_forces[body] = damping_force
+        up.damping_forces[Trunk] -= damping_force
     end
 
     # # Apply soft velocity limits
@@ -210,16 +257,17 @@ function update(model::BoxAtlas, x::State{T1}, u::Input{T2}) where {T1, T2}
     #     acceleration[Trunk] += -force ./ model.masses[Trunk]
     # end
 
-    # Non-inertial reference frame
-    for body in keys(u.force)
-        acceleration[body] -= acceleration[Trunk]
-    end
+    # # Non-inertial reference frame
+    # for body in keys(u.force)
+    #     acceleration[body] -= acceleration[Trunk]
+    # end
+    acceleration = accelerations(model, up)
 
     for body in [Trunk, keys(u.force)...]
         xnext.velocity[body] = x.velocity[body] .+ acceleration[body] .* model.Δt
         xnext.position[body] = x.position[body] .+ x.velocity[body] .* model.Δt .+ 0.5 .* acceleration[body] .* model.Δt.^2
     end
-    xnext
+    xnext, up, acceleration
 end
 
 function DrakeVisualizer.setgeometry!(vis::Visualizer, model::BoxAtlas)
@@ -228,6 +276,7 @@ function DrakeVisualizer.setgeometry!(vis::Visualizer, model::BoxAtlas)
     for body in (LeftFoot, RightFoot, LeftHand, RightHand)
         setgeometry!(vis[:trunk][Symbol(body)], HyperSphere(Point(0., 0, 0), 0.05))
     end
+    setgeometry!(vis[:environment][:right_wall], HyperRectangle(Vec(-0.01, -0.5, 0), Vec(0.02, 1.0, 1.5)))
 end
 
 function DrakeVisualizer.settransform!(vis::Visualizer, model::BoxAtlas, x::State)
@@ -272,26 +321,41 @@ function setlimits!(x::State{Variable}, robot::BoxAtlas)
     end
 end
 
-function run_mpc(robot::BoxAtlas, x0::State, N=10)
-    model = Model(solver=GurobiSolver())
+function run_mpc(robot::BoxAtlas, x0::State, N=10; 
+        solver=GurobiSolver(), 
+        xdesired=State([0.5, 0.9, 0.2, -0.9, -0.2, -0.9, 0.4, 0.1, -0.4, 0.1, zeros(10)...]))
+    model = Model(solver=solver)
     u = Input(@variable(model, [1:10], basename="u"))
     setlimits!(u, robot)
     x = State(@variable(model, [1:20], basename="x"))
     setlimits!(x, robot)
-    @constraint(model, vec(x) .== vec(update(robot, x0, u)))
+    xnext, up, accel = update(robot, x0, u)
+    @constraint(model, vec(x) .== vec(xnext))
     inputs = [u]
     states = [x]
+    updates = [up]
+    accels = [accel]
     for i in 2:N
         u = Input(@variable(model, [1:10], basename="u"))
         setlimits!(u, robot)
         x = State(@variable(model, [1:20], basename="x"))
         setlimits!(x, robot)
-        @constraint(model, vec(x) .== vec(update(robot, states[end], u)))
+        xnext, up, accel = update(robot, states[end], u)
+        @constraint(model, vec(x) .== vec(xnext))
         push!(inputs, u)
         push!(states, x)
+        push!(updates, up)
+        push!(accels, accel)
     end
     setup_indicators!(model)
-    @objective(model, Min, 0.1 * sum([sum(vec(u).^2) for u in inputs]) + 10 * sum((vec(states[end]) .- [0.5, 0.5, zeros(18)...]).^2))
+    obj = 0.1 * sum([sum(vec(u).^2) for u in inputs]) + 10 * sum((vec(states[end]) .- vec(xdesired)).^2)
+    for up in updates
+        for body in keys(u.force)
+            obj += up.ground_contact_forces[body][1]^2
+            obj += up.wall_contact_forces[body][2]^2
+        end
+    end
+    @objective(model, Min, obj)
     status = solve(model)
     getvalue.(inputs), [x0, getvalue.(states)...]
 end
--- experiments/2017-07-16-box-atlas-indicators/indicators.jl
+++ experiments/2017-07-16-box-atlas-indicators/indicators.jl
@@ -1,6 +1,7 @@
-
 using CoordinateTransformations
 using DrakeVisualizer
+using Gurobi
+
 DrakeVisualizer.any_open_windows() || DrakeVisualizer.new_window()
 
 include("boxatlas.jl")
@@ -10,7 +11,7 @@ model = Box.BoxAtlas(stiffness=1.0, damping=1.0, viscous_friction=100.)
 vis = Visualizer()[:boxatlas]
 setgeometry!(vis, model)
 
-state = Box.State(vcat(rand(2), randn(8), 1 * randn(10)))
+state = Box.State(vcat(rand(2), randn(8), 5 * randn(10)))
 input = Box.Input(zeros(8))
 model.stiffness = 10.
 model.damping = 10
@@ -20,20 +21,26 @@ for i in 1:1000
     settransform!(vis, model, state)
     model.stiffness += 10
     model.damping = 1.0 * sqrt(model.stiffness)
-    state = Box.update(model, state, input)
+    state, up, accel = Box.update(model, state, input)
     sleep(0.001)
 end
     
 
 state.position[Box.Trunk] = [0.5, state.position[Box.Trunk][2]]
-state.velocity[Box.Trunk] = [2.0, 1.0]
-# model.stiffness=2000
+state.velocity[Box.Trunk] = [-1.8, 0]
+model.stiffness=1000
 model.Δt = 0.05
-us, xs = Box.run_mpc(model, state, 5)
+us, xs = Box.run_mpc(model, state, 10, solver=GurobiSolver(TimeLimit=60))
 
 for x in xs
     settransform!(vis, model, x)
     sleep(0.1)
 end
 
+# state = xs[end]
+# us, xs = Box.run_mpc(model, state, 6, solver=GurobiSolver(TimeLimit=60))
 
+# for x in xs
+#     settransform!(vis, model, x)
+#     sleep(0.1)
+# end
